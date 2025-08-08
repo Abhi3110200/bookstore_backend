71c0de809bc137d154dd34b0eda4efefd71a001f
Abhi3110200 <abhijeetdrv@gmail.com>
2025-08-08 16:29:28 +0530

    Add the created in login route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -80,7 +80,8 @@ router.post("/login", async (req, res) => {
         _id : user._id,
         username : user.username,
         email : user.email,
-        profilePicture : user.profilePicture
+        profilePicture : user.profilePicture,
+        createdAt : user.createdAt
     }});
     
    }catch(error){
